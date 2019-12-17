f3e1bfe95e6ec38944a6dbed4abb5ab0535e46fa
Rob Rix <rob.rix@me.com>
2019-12-17 05:43:19 -0500

    Define a handler for ReadlineC.

--- src/Control/Carrier/Readline/Haskeline.hs
+++ src/Control/Carrier/Readline/Haskeline.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Readline.Haskeline
 ( -- * Readline carrier
-  ReadlineC(..)
+  runReadline
+, ReadlineC(..)
   -- * Readline effect
 , module Control.Effect.Readline
 ) where
@@ -10,12 +11,19 @@ import Control.Carrier.Lift
 import Control.Carrier.Reader
 import Control.Effect.Readline
 import Control.Monad.Fix (MonadFix)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Trans.Class (MonadTrans(..))
+import Data.Coerce (coerce)
 import System.Console.Haskeline
 
-newtype ReadlineC m a = ReadlineC (ReaderC Int (LiftC (InputT m)) a)
+runReadline :: MonadException m => Prefs -> Settings m -> ReadlineC m a -> m a
+runReadline prefs settings (ReadlineC m) = runInputTWithPrefs prefs (coerce settings) (runM (runReader (Line 0) m))
+
+newtype ReadlineC m a = ReadlineC (ReaderC Line (LiftC (InputT m)) a)
   deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
 
 instance MonadTrans ReadlineC where
   lift = ReadlineC . lift . lift . lift
+
+
+newtype Line = Line Int
