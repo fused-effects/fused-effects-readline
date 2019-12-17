a935156c11d4d73de9a6bf64b8bb265ec2a7baab
Rob Rix <rob.rix@me.com>
2019-12-17 05:30:31 -0500

    Define a Readline carrier.

--- src/Control/Carrier/Readline/Haskeline.hs
+++ src/Control/Carrier/Readline/Haskeline.hs
@@ -1,6 +1,13 @@
 module Control.Carrier.Readline.Haskeline
-( -- * Readline effect
-  module Control.Effect.Readline
+( -- * Readline carrier
+  ReadlineC(..)
+  -- * Readline effect
+, module Control.Effect.Readline
 ) where
 
+import Control.Carrier.Lift
+import Control.Carrier.Reader
 import Control.Effect.Readline
+import System.Console.Haskeline
+
+newtype ReadlineC m a = ReadlineC (ReaderC Int (LiftC (InputT m)) a)
