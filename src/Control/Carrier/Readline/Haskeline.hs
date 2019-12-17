0a853c5928cfaf2751d2d025ff88ef0046d80845
Rob Rix <rob.rix@me.com>
2019-12-17 05:48:35 -0500

    Add a handler to run with history.
    NB: This makes assumptions about where things should go which are probably incorrect for Windows.

--- src/Control/Carrier/Readline/Haskeline.hs
+++ src/Control/Carrier/Readline/Haskeline.hs
@@ -2,6 +2,7 @@
 module Control.Carrier.Readline.Haskeline
 ( -- * Readline carrier
   runReadline
+, runReadlineWithHistory
 , ReadlineC(..)
   -- * Readline effect
 , module Control.Effect.Readline
@@ -15,9 +16,27 @@ import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Trans.Class (MonadTrans(..))
 import Data.Coerce (coerce)
 import System.Console.Haskeline
+import System.Directory
+import System.Environment
+import System.FilePath
 
 runReadline :: MonadException m => Prefs -> Settings m -> ReadlineC m a -> m a
 runReadline prefs settings (ReadlineC m) = runInputTWithPrefs prefs (coerce settings) (runM (runReader (Line 0) m))
+
+runReadlineWithHistory :: MonadException m => ReadlineC m a -> m a
+runReadlineWithHistory block = do
+  homeDir <- liftIO getHomeDirectory
+  prefs   <- liftIO $ readPrefs (homeDir </> ".haskeline")
+  prog    <- liftIO getExecutablePath
+  let settingsDir = homeDir </> ".local" </> dropExtension (takeFileName prog)
+      settings = Settings
+        { complete = noCompletion
+        , historyFile = Just (settingsDir </> "repl_history")
+        , autoAddHistory = True
+        }
+  liftIO $ createDirectoryIfMissing True settingsDir
+
+  runReadline prefs settings block
 
 newtype ReadlineC m a = ReadlineC (ReaderC Line (LiftC (InputT m)) a)
   deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
