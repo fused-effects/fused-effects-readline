9e39823744266e7c35e22c2b12538d39b600f77b
Rob Rix <rob.rix@me.com>
2019-12-17 05:24:40 -0500

    Define a smart constructor for Prompt.

--- src/Control/Effect/Readline.hs
+++ src/Control/Effect/Readline.hs
@@ -2,6 +2,7 @@
 module Control.Effect.Readline
 ( -- * Readline effect
   Readline(..)
+, prompt
   -- * Re-exports
 , Algebra
 , Has
@@ -12,6 +13,10 @@ import Control.Algebra
 import Data.Text.Prettyprint.Doc
 import Data.Text.Prettyprint.Doc.Render.Terminal
 import GHC.Generics (Generic1)
+
+prompt :: Has Readline sig m => String -> m (Int, Maybe String)
+prompt p = send (Prompt p (curry pure))
+
 
 data Readline m k
   = Prompt String (Int -> Maybe String -> m k)
