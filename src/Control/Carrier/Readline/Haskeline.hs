b04a6d9abfe75eb80106f5aaa6b93769b6c2f93f
Rob Rix <rob.rix@me.com>
2019-12-17 05:39:50 -0500

    Define a MonadTrans instance for ReadlineC.

--- src/Control/Carrier/Readline/Haskeline.hs
+++ src/Control/Carrier/Readline/Haskeline.hs
@@ -11,7 +11,11 @@ import Control.Carrier.Reader
 import Control.Effect.Readline
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Class (MonadTrans(..))
 import System.Console.Haskeline
 
 newtype ReadlineC m a = ReadlineC (ReaderC Int (LiftC (InputT m)) a)
   deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
+
+instance MonadTrans ReadlineC where
+  lift = ReadlineC . lift . lift . lift
